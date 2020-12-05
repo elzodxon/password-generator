2b8adb9cb539889f86e82623784d8cae89cb3e10
elzodxon <sharofaddinov01@mail.ru>
2020-12-05 15:55:44 +0500

    Added clipboard

---
+++ app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    Password Generator
+    <Form/>
+    <Preference/>
+  </div>
+</template>
+
+<script>
+    import Form from '@/components/Form'
+    import Preference from '@/components/Preference'
+
+    export default {
+      name: 'App',
+      components: {
+        Form,
+        Preference
+      }
+    }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <form action="">
+      <input id="foo" type="text" readonly value="https://github.com/zenorocha/clipboard.js.git">
+      <button data-clipboard-target="#foo" class="btn">Copy</button>
+      <button>Generate password</button>
+    </form>
+
+    <hr>
+  </div>
+</template>
+
+<script>
+var clipboard = new Clipboard('.btn');
+
+clipboard.on('success', function(e) {
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+
+  e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+  console.error('Action:', e.action);
+  console.error('Trigger:', e.trigger);
+});
+</script>
+
+<style scoped>
+
+</style>
